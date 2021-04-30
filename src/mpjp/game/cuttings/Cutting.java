a663a2d52ff85e21ba25f2e02f786143efe1c1cc
PatriciaV24 <pdtvieira@hotmail.com>
2021-04-30 14:41:10 +0100

    corrr

--- src/mpjp/game/cuttings/Cutting.java
+++ src/mpjp/game/cuttings/Cutting.java
@@ -6,6 +6,7 @@ import mpjp.shared.geom.PieceShape;
 
 public interface Cutting {
 	Map<Integer, PieceShape> getShapes(PuzzleStructure structure){
+		//Produce a map of piece ID (Integer) to PieceShape
 		
 	}
 }
