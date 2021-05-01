fd070c5d6a06a216156f3cae71806530fd5cddfb
manelsa1912 <manelsa1912@gmail.com>
2021-05-01 12:06:33 +0100

    por favor verifica as tasks

--- src/mpjp/game/cuttings/StraightCutting.java
+++ src/mpjp/game/cuttings/StraightCutting.java
@@ -1,21 +1,36 @@
 package mpjp.game.cuttings;
 
+import java.awt.Point;
 import java.util.HashMap;
 import java.util.Map;
 
 import mpjp.game.PuzzleStructure;
 import mpjp.shared.geom.PieceShape;
 
-public class StraightCutting 
-implements Cutting{
+public class StraightCutting implements Cutting {
 
 	public StraightCutting() {
 		super();
 	}
-	
-	public Map<Integer,PieceShape> getShapes(PuzzleStructure structure){
-		 Map<Integer,PieceShape> map = new HashMap<Integer,PieceShape>();
-		 
-		 return map;
+
+	public Map<Integer, PieceShape> getShapes(PuzzleStructure structure) {
+		Map<Integer, PieceShape> map = new HashMap<Integer, PieceShape>();
+
+		double centerPointX = structure.getPieceWidth() / 2;
+		double centerPointY = structure.getPieceHeight() / 2;
+
+		// TODO ver porque e que os pontos nao sao criados
+		Point topRight = new Point(centerPointX, centerPointY);
+		Point TopLeft = new Point(-centerPointX, centerPointY);
+
+		Point BottomRight = new Point(centerPointX, -centerPointY);
+		Point BottomLeft = new Point(-centerPointX, -centerPointY);
+
+		PieceShape piece = new PieceShape();
+		// TODO adicionar os pontos ao piece
+
+		for (int i = 0; i < structure.getPieceCount(); i++)
+			map.put(i, piece);
+		return map;
 	}
 }
