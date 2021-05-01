3fba7d923701c4d0ad968438c3ca42691a157f31
manelsa1912 <manelsa1912@gmail.com>
2021-05-01 10:44:05 +0100

    so falta as formas para acabar o mpjp.game.cuttings

--- src/mpjp/game/cuttings/StandardCutting.java
+++ src/mpjp/game/cuttings/StandardCutting.java
@@ -1,5 +1,51 @@
 package mpjp.game.cuttings;
 
-public class StandardCutting {
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.Map;
 
+import mpjp.game.Direction;
+import mpjp.game.PuzzleStructure;
+import mpjp.shared.geom.PieceShape;
+
+public class StandardCutting 
+implements Cutting{
+
+	public StandardCutting() {
+		super();
+	}
+	
+	public Map<Integer,PieceShape> getShapes(PuzzleStructure structure){
+		 Map<Integer,PieceShape> map = new HashMap<Integer,PieceShape>();
+		 
+		 return map;
+	}
+	
+	Point getMiddlePoint(int id,Direction direction){
+		Point p = new Point();
+		return p;	
+	}
+	
+	
+	Point getStartControlPoint1(int id,Direction direction){
+		Point p = new Point();
+		return p;	
+	}
+	
+	Point getStartControlPoint2(int id,Direction direction){
+		Point p = new Point();
+		return p;	
+	}
+	
+	Point getEndControlPoint1(int id,Direction direction){
+		Point p = new Point();
+		return p;	
+	}
+	
+	Point getEndControlPoint2(int id,Direction direction){
+		Point p = new Point();
+		return p;	
+	}
+	
+	
 }
--- src/mpjp/game/cuttings/TriangularCutting.java
+++ src/mpjp/game/cuttings/TriangularCutting.java
@@ -1,5 +1,22 @@
 package mpjp.game.cuttings;
 
-public class TriangularCutting {
+import java.util.HashMap;
+import java.util.Map;
 
+import mpjp.game.PuzzleStructure;
+import mpjp.shared.geom.PieceShape;
+
+public class TriangularCutting
+implements Cutting{
+
+	public TriangularCutting() {
+		super();
+		
+	}
+	
+	public Map<Integer,PieceShape> getShapes(PuzzleStructure structure){
+		 Map<Integer,PieceShape> map = new HashMap<Integer,PieceShape>();
+		 
+		 return map;
+	}
 }
