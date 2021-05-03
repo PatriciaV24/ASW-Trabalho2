3c32cb782b4ab9b8eee9f1fda8d47b930266deda
manelsa1912 <72703775+manelsa1912@users.noreply.github.com>
2021-05-03 01:20:36 +0100

    Update WorkspacePool.java

--- src/mpjp/game/WorkspacePool.java
+++ src/mpjp/game/WorkspacePool.java
@@ -8,103 +8,102 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import mpjp.shared.MPJPException;
 import mpjp.shared.PuzzleInfo;
 import mpjp.shared.PuzzleSelectInfo;
 
 public class WorkspacePool {
-	static final String SERIALIAZTION_SUFFIX = ".ser";
-	private static File path;
-	private Map<String, PuzzleSelectInfo> availableWorkspaces;
 
-	public WorkspacePool() {
-		super();
-		this.availableWorkspaces = new HashMap<String, PuzzleSelectInfo>();
-	}
-
-	void backup(String workspaceId, Workspace workspace) {
-		try {
-			FileOutputStream fileOut =
-			         new FileOutputStream(workspace.getId()+SERIALIAZTION_SUFFIX);
-			 ObjectOutputStream out = new ObjectOutputStream(fileOut);
-	         out.writeObject(workspace);
-	         out.close();
-	         fileOut.close();
-	        
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
+	static final java.lang.String SERIALIAZTION_SUFFIX = ".ser";
+	private static File PoolDirectory;
 	
-
-	String createWorkspace(PuzzleInfo info) {
-		Workspace workspace;
-		try {
-			workspace = new Workspace(info);
-			FileOutputStream fileOut =
-			         new FileOutputStream(workspace.getId()+SERIALIAZTION_SUFFIX);
-			 ObjectOutputStream out = new ObjectOutputStream(fileOut);
-	         out.writeObject(workspace);
-	         out.close();
-	         fileOut.close();
-	         return workspace.getId();
-		} catch (IOException | MPJPException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	private static Map<String, Workspace> workspaces;
+	private static Map<String, File> files;
+	
+	public WorkspacePool() {
+		workspaces = new HashMap<String, Workspace>();
+		files = new HashMap<String, File>();
+		PoolDirectory= new File(".");
+	}
+	
+	public static File getPoolDirectory() {
+		return PoolDirectory;
+	}
+	
+	public static void setPoolDiretory​(File poolDirectory) {
+		WorkspacePool.PoolDirectory = poolDirectory;
+	}
+	
+	public static void setPoolDiretory​(String pathname) {
+		PoolDirectory = new File(pathname);
+	}
+	
+	String createWorkspace​(PuzzleInfo info) throws MPJPException {
+		Workspace workspace = new Workspace(info);
+		workspaces.put(workspace.getId(), workspace);
+		try{
+			backup​(workspace.getId(), workspace);
+			return workspace.getId();
+		}catch(Exception e) {
+			throw new MPJPException("Not Serializable");
 		}
-		return null;
 	}
-
-	Map<String, PuzzleSelectInfo> getAvailableWorkspaces() {
-		return this.availableWorkspaces;
+	
+	Workspace getWorkspace​(String id) throws MPJPException {
+		return workspaces.get(id);
 	}
-
-	File getFile(String id) {
-		for (File x : getPoolDirectory().listFiles()) {
-			if (x.getName().startsWith(id)) {
-				return x;
-			}
+	
+	Map<String, PuzzleSelectInfo> getAvailableWorkspaces(){
+		
+		Map<String, PuzzleSelectInfo> info = new HashMap<String, PuzzleSelectInfo>();
+		for(Entry<String, Workspace> workspace : workspaces.entrySet()) {
+			info.put(workspace.getKey(), workspace.getValue().getPuzzleSelectInfo());
 		}
-		return null;
+		return info;
 	}
-
-	static File getPoolDirectory() {
-		return path;
+	
+	private File CreateWorkspaceFile(String workspaceId) {
+		return new File(PoolDirectory, workspaceId + SERIALIAZTION_SUFFIX);
 	}
-
-	Workspace recover(String workspaceId) {
-		Workspace workspace = null;
-		if(getPoolDirectory().canRead()){
-			try(
-				FileInputStream stream = new FileInputStream(getPoolDirectory());
-				ObjectInputStream deserializer = new ObjectInputStream(stream);
-					){
-				workspace = (Workspace) deserializer.readObject();
-					} catch(IOException | ClassNotFoundException cause){
-						cause.printStackTrace();
-						}
+	
+	File getFile​(String workspaceId) {
+		if(! files.containsKey(workspaceId)) {
+			files.put(workspaceId, CreateWorkspaceFile(workspaceId));
+		}
+		return files.get(workspaceId);
+	}
+	
+	void backup​(String workspaceId, Workspace workspace) throws MPJPException {
+	    try(
+	    	FileOutputStream stream = new FileOutputStream(getFile​(workspaceId));
+	        ObjectOutputStream serializer = new ObjectOutputStream(stream);
+	        ) { 
+	            serializer.writeObject(workspace);
+	    } catch(IOException cause) {
+	    	
+	    }
+	}
+	
+	Workspace recover​(String workspaceId) throws MPJPException {
+		Workspace space = null;
+		try(
+			FileInputStream stream = new FileInputStream(getFile​(workspaceId));
+			ObjectInputStream deserializer = new ObjectInputStream(stream);
+			) { 
+			 	space = (Workspace) deserializer.readObject();
+			 	space.restore();
+				workspaces.put(workspaceId, space);
+		} catch(IOException | ClassNotFoundException cause) {
+			    System.out.println(cause);
 		}
 		
-		//else {
-		//	workspace = new Workspace();
-		//			}
-		return workspace;
+		return space;
 	}
-
+	
 	void reset() {
-		this.availableWorkspaces = null;
-	}
-
-	static void setPoolDirectory(File PoolDirectory) {
-		WorkspacePool.path = PoolDirectory;
-
-	}
-
-	static void setPoolDirectory(String pathname) {
-		File nPath = new File(pathname);
-		WorkspacePool.path = nPath;
+		PoolDirectory = new File(".");
+		workspaces =new HashMap<String, Workspace>();
 	}
 }
